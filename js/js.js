a7e336891f1905afd10487a9118bbd363db0a01d
Taylor Welling <tw3080@gmail.com>
2016-05-23 18:17:04 -0700

    optimization

--- js/js.js
+++ js/js.js
@@ -1,14 +1,13 @@
 function fizzBuzz() {
   for (i = 1; i <= 100; i++) {
-    if ((i % 3 === 0) && (i % 5 === 0)) {
-      $("body").append('fizz buzz<br>');
-    } else if (i % 3 === 0) {
-      $("body").append('fizz<br>');
-    } else if (i % 5 === 0) {
-      $("body").append('buzz<br>');
-    } else {
-      $("body").append(i + '<br>');
+    var result = "";
+    if (i % 3 === 0) {
+      result += "fizz";
     }
+    if (i % 5 === 0) {
+      result += "buzz";
+    }
+    $("body").append((result || i) + "<br>");
   }
 }
 
